f8a1e4446187ef86d1477332f76a8b5e03b0cc5a
hetviboda18 <hetviboda02@gmail.com>
2026-06-29 11:20:12 +0530

    hello

---
+++ Date.js
@@ -0,0 +1,19 @@
+let myDate= new Date()
+// console.log(myDate.toString());
+// console.log(myDate.toJSON());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toLocaleString());
+// console.log(typeof myDate);
+// let CreateDate= new Date(2025,0,2,13,40)
+ let CreateDate= new Date("2026-02-18 5:50")
+// let CreateDate= new Date(2025,0,2,13,40)
+//console.log(CreateDate.toLocaleString());
+let myTime=Date.now()
+console.log(myTime);
+console.log(CreateDate.getTime());
+console.log(`my time is ${myTime}`)
+
+
+
+
---
+++ Number.js
@@ -0,0 +1,16 @@
+const score=100000
+const balance=new Number(100)
+// console.log(balance);
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(5));
+const OtherNum=56.89374
+console.log(OtherNum.toPrecision(6));
+console.log(score.toLocaleString('en-In'));
+console.log("hello my name is ${score}");
+
+console.log(Math.abs(-6));
+console.log(parseInt(Math.random()*10));
+
+console.log(Math.sqrt(4));
+
+
